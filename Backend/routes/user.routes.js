3c7f0f715f14591addd1c31892706ffa4e5a209b
Krunal Kurkure <adkurkure@gmail.com>
2025-09-18 19:27:10 +0530

    User Profie Login route

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
 const userController = require("../controllers/user.controllers");
-
+const authMiddleware = require("../middlewares/auth.middlewares");
 router.post(
   "/register",
   [
@@ -27,4 +27,6 @@ router.post(
   ],
   userController.loginUser
 );
+
+router.get("/profile", authMiddleware.authUser, userController.getUserProfile);
 module.exports = router;
