380622237139b46f1b291577f6d7e9c9371e929b
Krunal Kurkure <adkurkure@gmail.com>
2025-10-03 21:50:27 +0530

    starting desining of home screen

---
+++ frontend/src/pages/CaptainLogin.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CaptainLogin = () => {
+  return <div>Captainlogin</div>;
+};
+
+export default CaptainLogin;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="h-screen pt-8 flex justify-between flex-col bg-red-400 w-full">
+        <img className="w-14 ml-8" src="https:/upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+        <div className="bg-white py-5 px-10">
+          <h2 className="text-2xl font-bold">Get Started with Uber</h2>
+        <button className="w-full bg-black text-white">Continue</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
