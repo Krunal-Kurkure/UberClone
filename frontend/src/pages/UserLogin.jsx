cb13db5a42375e49ad9e616d0192b2bf7b795488
Krunal Kurkure <adkurkure@gmail.com>
2025-10-04 17:46:26 +0530

    User Login and Home page Design Done

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { Link, Links } from "react-router-dom";
 
 const UserLogin = () => {
-  return <div>UserLogin</div>;
+  return (
+    <div className="p-7 h-screen flex flex-col justify-between">
+       <div>
+         <img
+          className="w-16 mb-10"
+          src="https:/upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt="uberlogo"
+        />
+      <form action="">
+        <h3 className="text-xl mb-3">What's your email</h3>
+        <input
+          className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+          required
+          type="email"
+          placeholder="email@example.com"
+          name=""
+          id=""
+        />
+        <h3 className="text-xl mb-3">Enter Password</h3>
+        <input
+          className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+          required
+          type="password"
+          placeholder="password"
+          name=""
+          id=""
+        />
+        <button className="bg-[#111] mb-7 text-white  font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base">
+          Login
+        </button>
+
+      </form>
+        <p className="text-center">New here? <Link to='/signup' className="text-[#394971] ">Create new Account</Link></p>
+       </div>
+
+       <div>
+        <button className="bg-[#394971] mb-7 text-white  font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base"> Sign in as Captain</button>
+       </div>
+    </div>
+  );
 };
 
 export default UserLogin;
