dfee4317c836d6c667dfea53c8d7f1f418573a88
Krunal Kurkure <adkurkure@gmail.com>
2025-10-07 22:05:34 +0530

    user and Captain login page is ready

--- frontend/src/pages/CaptainLogin.jsx
+++ frontend/src/pages/CaptainLogin.jsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const CaptainLogin = () => {
-  return <div>Captainlogin</div>;
+  const [email, setemail] = useState("");
+    const [password, setpassword] = useState("");
+    const [captaindata, setcaptaindata] = useState({});
+  
+    const submitHandler = (e) => {
+      e.preventDefault();
+  
+      setcaptaindata({
+        email: email,
+        password: password,
+      });
+  
+      setemail("");
+      setpassword("");
+    };
+
+  return (
+      <div className="p-7 h-screen flex flex-col justify-between">
+      <div>
+        <img
+          className="w-16 mb-10"
+          src="https:/upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt="uberlogo"
+        />
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+        >
+          <h3 className="text-xl mb-3">What's your email</h3>
+          <input
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            required
+            value={email}
+            onChange={(e) => {
+              setemail(e.target.value);
+            }}
+            type="email"
+            placeholder="email@example.com"
+            name=""
+            id=""
+          />
+          <h3 className="text-xl mb-3">Enter Password</h3>
+          <input
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            required
+            value={password}
+            onChange={(e) => {
+              setpassword(e.target.value);
+            }}
+            type="password"
+            placeholder="password"
+            name=""
+            id=""
+          />
+          <button className="bg-[#111] mb-7 text-white  font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base">
+            Login
+          </button>
+        </form>
+        <p className="text-center">
+          join a fleet <Link to="/signup" className="text-[#85002a] ">
+            Register as a Captain
+          </Link>
+        </p>
+      </div>
+
+      <div>
+        <Link to='/captain-login' className="bg-[#85002a] mb-6 flex items-center justify-center text-white  font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base">
+          Sign in as User
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default CaptainLogin;
--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,45 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Links } from "react-router-dom";
 
 const UserLogin = () => {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [userdata, setuserdata] = useState({});
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setuserdata({
+      email: email,
+      password: password,
+    });
+
+    setemail("");
+    setpassword("");
+  };
+
   return (
     <div className="p-7 h-screen flex flex-col justify-between">
-       <div>
-         <img
+      <div>
+        <img
           className="w-16 mb-10"
           src="https:/upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
           alt="uberlogo"
         />
-      <form action="">
-        <h3 className="text-xl mb-3">What's your email</h3>
-        <input
-          className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
-          required
-          type="email"
-          placeholder="email@example.com"
-          name=""
-          id=""
-        />
-        <h3 className="text-xl mb-3">Enter Password</h3>
-        <input
-          className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
-          required
-          type="password"
-          placeholder="password"
-          name=""
-          id=""
-        />
-        <button className="bg-[#111] mb-7 text-white  font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base">
-          Login
-        </button>
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+        >
+          <h3 className="text-xl mb-3">What's your email</h3>
+          <input
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            required
+            value={email}
+            onChange={(e) => {
+              setemail(e.target.value);
+            }}
+            type="email"
+            placeholder="email@example.com"
+            name=""
+            id=""
+          />
+          <h3 className="text-xl mb-3">Enter Password</h3>
+          <input
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            required
+            value={password}
+            onChange={(e) => {
+              setpassword(e.target.value);
+            }}
+            type="password"
+            placeholder="password"
+            name=""
+            id=""
+          />
+          <button className="bg-[#111] mb-7 text-white  font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base">
+            Login
+          </button>
+        </form>
+        <p className="text-center">
+          New here?{" "}
+          <Link to="/signup" className="text-[#394971] ">
+            Create new Account
+          </Link>
+        </p>
+      </div>
 
-      </form>
-        <p className="text-center">New here? <Link to='/signup' className="text-[#394971] ">Create new Account</Link></p>
-       </div>
-
-       <div>
-        <button className="bg-[#394971] mb-7 text-white  font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base"> Sign in as Captain</button>
-       </div>
+      <div>
+        <Link to='/captain-login' className="bg-[#394971] mb-6 flex items-center justify-center text-white  font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base">
+          Sign in as Captain
+        </Link>
+      </div>
     </div>
   );
 };
