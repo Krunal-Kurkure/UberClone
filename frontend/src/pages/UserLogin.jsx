05911741ec3d002d90d44075b27b97aa762370a7
Krunal Kurkure <adkurkure@gmail.com>
2025-10-15 22:11:37 +0530

    here user login and register full logic implemented

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,18 +1,41 @@
-import React, { useState } from "react";
-import { Link, Links } from "react-router-dom";
+import React, { useState,useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { UserDataContext } from "../../src/context/UserContext";
 
 const UserLogin = () => {
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
-  const [userdata, setuserdata] = useState({});
 
-  const submitHandler = (e) => {
+  const navigate = useNavigate();
+    const { user, setuser } = useContext(UserDataContext);
+
+  const submitHandler = async (e) => {
     e.preventDefault();
 
-    setuserdata({
+       const userData = {
       email: email,
       password: password,
-    });
+    };
+
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BASE_URL}/users/login`,
+        userData
+      );
+      if (response.status === 200) {
+        setuser(response.data.user);
+        navigate("/home");
+      }
+    } catch (err) {
+      if (err.response?.status === 409) {
+        alert("Email already exists");
+      } else {
+        console.error("Signup error:", err);
+        alert("Something went wrong. Please try again.");
+      }
+    }
+
 
     setemail("");
     setpassword("");
@@ -31,10 +54,11 @@ const UserLogin = () => {
             submitHandler(e);
           }}
         >
-          <h3 className="text-xl mb-3">What's your email</h3>
+          <h3 className="text-xl mb-3">What's your emaill</h3>
           <input
             className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
             required
+           
             value={email}
             onChange={(e) => {
               setemail(e.target.value);
@@ -42,20 +66,20 @@ const UserLogin = () => {
             type="email"
             placeholder="email@example.com"
             name=""
-            id=""
+             id="email"
           />
           <h3 className="text-xl mb-3">Enter Password</h3>
           <input
             className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
             required
+            
             value={password}
             onChange={(e) => {
               setpassword(e.target.value);
             }}
             type="password"
             placeholder="password"
-            name=""
-            id=""
+           id="password"
           />
           <button className="bg-[#111] mb-7 text-white  font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base">
             Login
--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,50 +1,51 @@
-import React, { useState } from "react";
-import { Link,useNavigate } from "react-router-dom";
-import axios from 'axios';
-import {UserDataContext} from '../../src/context/UserContext';
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { UserDataContext } from "../../src/context/UserContext";
 
 const UserSignup = () => {
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
-  const [firstName, setfirstName] = useState('');
-  const [lastName, setlastName] = useState('');
+  const [firstName, setfirstName] = useState("");
+  const [lastName, setlastName] = useState("");
 
   const navigate = useNavigate();
-  const {user, setuser} = React.useContext(UserDataContext);
+  const { user, setuser } = useContext(UserDataContext);
 
   const submitHandler = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const newUser = {
-      fullname:{
-        firstname:firstName,
-        lastname:lastName,
+      fullname: {
+        firstname: firstName,
+        lastname: lastName,
       },
       email: email,
       password: password,
     };
 
-  try {
-  const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/register`, newUser);
-  if (response.status === 201) {
-    setuser(response.data.user);
-    navigate('/home');
-  }
-} catch (err) {
-  if (err.response?.status === 409) {
-    alert("Email already exists");
-  } else {
-    console.error("Signup error:", err);
-    alert("Something went wrong. Please try again.");
-  }
-}
-
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BASE_URL}/users/register`,
+        newUser
+      );
+      if (response.status === 201) {
+        setuser(response.data.user);
+        navigate("/home");
+      }
+    } catch (err) {
+      if (err.response?.status === 409) {
+        alert("Email already exists");
+      } else {
+        console.error("Signup error:", err);
+        alert("Something went wrong. Please try again.");
+      }
+    }
 
     setemail("");
     setpassword("");
     setfirstName("");
     setlastName("");
   };
-
 
   return (
     <div className="p-7 h-screen flex flex-col justify-between">
@@ -59,29 +60,28 @@ const UserSignup = () => {
             submitHandler(e);
           }}
         >
-
-           <h3 className="text-xl mb-3">What's your name</h3>
+          <h3 className="text-xl mb-3">What's your name</h3>
           <div className="flex gap-4 mb-7">
             <input
-            className="bg-[#eeeeee] w-1/2  rounded px-4 py-2 border  text-lg placeholder:text-base"
-            required
-            type="text"
-            value={firstName}
-            onChange={(e)=>{
-              setfirstName(e.target.value)
-            }}
-            placeholder="Firstname"
-          />
-           <input
-            className="bg-[#eeeeee] w-1/2  rounded px-4 py-2 border  text-lg placeholder:text-base"
-            required
-            type="text"
+              className="bg-[#eeeeee] w-1/2  rounded px-4 py-2 border  text-lg placeholder:text-base"
+              required
+              type="text"
+              value={firstName}
+              onChange={(e) => {
+                setfirstName(e.target.value);
+              }}
+              placeholder="Firstname"
+            />
+            <input
+              className="bg-[#eeeeee] w-1/2  rounded px-4 py-2 border  text-lg placeholder:text-base"
+              required
+              type="text"
               value={lastName}
-            onChange={(e)=>{
-              setlastName(e.target.value)
-            }}
-            placeholder="Lastname"
-          />
+              onChange={(e) => {
+                setlastName(e.target.value);
+              }}
+              placeholder="Lastname"
+            />
           </div>
 
           <h3 className="text-xl mb-3">What's your email</h3>
@@ -107,17 +107,25 @@ const UserSignup = () => {
             }}
             type="password"
             placeholder="password"
-                 />
+          />
           <button className="bg-[#111] mb-7 text-white  font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base">
-          Create account
+            Create account
           </button>
         </form>
-        <p className="text-center">Already have a account? <Link to="/login" className="text-[#394971] ">Login here</Link>
+        <p className="text-center">
+          Already have a account?{" "}
+          <Link to="/login" className="text-[#394971] ">
+            Login here
+          </Link>
         </p>
       </div>
 
       <div>
-        <p className="text-xs leading-tight">By proceeding you consent to get calls whatsapp or sms messages, including by automated means, from Uber and its affiliates to number provided.</p>
+        <p className="text-xs leading-tight">
+          By proceeding you consent to get calls whatsapp or sms messages,
+          including by automated means, from Uber and its affiliates to number
+          provided.
+        </p>
       </div>
     </div>
   );
